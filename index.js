578a8453a0eead07fb79a7ef7c93b24d67174267
Mira E <miraelhajj0@gmail.com>
2025-04-19 20:58:56 +0300

    Starting the JS and creating function addTask

---
+++ index.js
@@ -0,0 +1,10 @@
+let tasks = [];
+
+
+const taskInput = document.getElementById("taskToAdd");
+const taskList = document.getElementById("taskList");
+
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+    if (taskText === "") return; }
